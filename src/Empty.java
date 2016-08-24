72097c763c41c444b7fd89936351bfbaca263721
sdarekar <sdarekar@admin-PC>
2016-08-24 09:44:24 +0530

    first commit

---
+++ src/Empty.java
@@ -0,0 +1,14 @@
+
+public class Empty {
+	
+	public Empty(int no,String name)
+	{
+		System.out.println("your account no is"+ no);
+		System.out.println("section name is"+name);
+	}
+	
+	public static void main(String []args)
+	{
+		Empty oldEmpty=new Empty(24,"computer");
+	}
+}
---
+++ src/Salary.java
@@ -0,0 +1,17 @@
+
+public class Salary {
+	
+	public Salary(String name,int num)
+	{
+		System.out.println("Employee name is:"+ name);
+		System.out.println("Your salary is"+ num);
+		
+	}
+
+	
+	public static void main(String []args)
+	{
+		Salary halfSalary= new Salary("Sayli",20000);
+		
+	}
+}
